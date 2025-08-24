d07cf7a24bb947f18ef86e32b069d4d83974e8fd
Zane Schepke <dev@zaneschepke.com>
2025-08-24 01:35:26 -0400

    chore: bump deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,20 +17,6 @@ dependencyResolutionManagement {
 	}
 }
 
-fun getLocalProperty(key: String, file: String = "local.properties"): String? {
-	val properties = java.util.Properties()
-	val localProperties = File(file)
-	if (localProperties.isFile) {
-		java.io.InputStreamReader(java.io.FileInputStream(localProperties), Charsets.UTF_8)
-			.use { reader ->
-				properties.load(reader)
-			}
-	} else {
-		return null
-	}
-	return properties.getProperty(key)
-}
-
 rootProject.name = "WG Tunnel"
 
 include(":app")
