ae711a3139a024ab2f2d8a442620db3e7aca9817
Zane Schepke <dev@zaneschepke.com>
2025-09-22 19:55:53 -0400

    chore: bump app verison with notes

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
-	const val VERSION_NAME = "3.9.5"
-    const val VERSION_CODE = 39500
+	const val VERSION_NAME = "4.0.0"
+    const val VERSION_CODE = 40000
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
