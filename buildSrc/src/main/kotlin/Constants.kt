14a7278747dc76908239d25a7788b0369410a3d1
Zane Schepke <dev@zaneschepke.com>
2025-09-25 01:43:57 -0400

    chore: bump for patch version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
-	const val VERSION_NAME = "4.0.0"
-    const val VERSION_CODE = 40000
+	const val VERSION_NAME = "4.0.1"
+    const val VERSION_CODE = 40001
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
