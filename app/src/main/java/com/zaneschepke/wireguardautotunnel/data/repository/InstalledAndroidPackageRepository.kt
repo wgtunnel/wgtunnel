68ab3fdc52c3ec905ce1b8cab6462b0ec3f447d6
Zane Schepke <dev@zaneschepke.com>
2025-10-28 22:36:12 -0400

    fix: split tunnel missing packages
    closes #750

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/data/repository/InstalledAndroidPackageRepository.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/data/repository/InstalledAndroidPackageRepository.kt
@@ -1,20 +1,15 @@
 package com.zaneschepke.wireguardautotunnel.data.repository
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import com.zaneschepke.wireguardautotunnel.di.ApplicationScope
 import com.zaneschepke.wireguardautotunnel.di.IoDispatcher
 import com.zaneschepke.wireguardautotunnel.domain.model.InstalledPackage
 import com.zaneschepke.wireguardautotunnel.domain.repository.InstalledPackageRepository
-import com.zaneschepke.wireguardautotunnel.util.extensions.getAllInternetCapablePackages
 import com.zaneschepke.wireguardautotunnel.util.extensions.getFriendlyAppName
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 
@@ -27,32 +22,6 @@ class InstalledAndroidPackageRepository(
 
     private var cachedPackages: List<InstalledPackage>? = null
 
-    init {
-        val receiver =
-            object : BroadcastReceiver() {
-                override fun onReceive(context: Context, intent: Intent) {
-                    when (intent.action) {
-                        Intent.ACTION_PACKAGE_ADDED,
-                        Intent.ACTION_PACKAGE_REMOVED,
-                        Intent.ACTION_PACKAGE_CHANGED -> {
-                            // don't update if we have nothing cached
-                            if (cachedPackages == null) return
-                            Timber.d("Updating installed packages cache")
-                            applicationScope.launch { refreshInstalledPackages() }
-                        }
-                    }
-                }
-            }
-        val filter =
-            IntentFilter().apply {
-                addAction(Intent.ACTION_PACKAGE_ADDED)
-                addAction(Intent.ACTION_PACKAGE_REMOVED)
-                addAction(Intent.ACTION_PACKAGE_CHANGED)
-                addDataScheme("package")
-            }
-        context.registerReceiver(receiver, filter)
-    }
-
     override suspend fun getInstalledPackages(): List<InstalledPackage> =
         withContext(ioDispatcher) {
             cachedPackages?.let {
@@ -63,7 +32,7 @@ class InstalledAndroidPackageRepository(
 
     override suspend fun refreshInstalledPackages(): List<InstalledPackage> =
         withContext(ioDispatcher) {
-            val packages = context.getAllInternetCapablePackages()
+            val packages = context.packageManager.getInstalledPackages(0)
 
             val installedPackages =
                 packages.mapNotNull { packageInfo ->
