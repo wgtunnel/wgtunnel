9b3283a2b13a3136e82bcba15ce11d2dccf260dc
Zane Schepke <dev@zaneschepke.com>
2025-11-04 20:20:41 -0500

    chore: release 4.1.4

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
-	const val VERSION_NAME = "4.1.3"
-    const val VERSION_CODE = 40103
+	const val VERSION_NAME = "4.1.4"
+    const val VERSION_CODE = 40104
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
